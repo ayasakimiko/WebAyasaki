3639ba90e91f557a67a197e47ff4762f77733912
ayasakimiko <zxz1470000@gmail.com>
2025-06-09 17:03:03 +0700

    Update zoom.js

--- js/zoom.js
+++ js/zoom.js
@@ -9,3 +9,12 @@ window.addEventListener('keydown', function(e) {
   }
 });
 document.querySelector('.profile-center').classList.add('visible');
+
+function resizeElement() {
+  const fullScreenDiv = document.getElementById("fullscreen");
+  fullScreenDiv.style.width = window.innerWidth + "px";
+  fullScreenDiv.style.height = window.innerHeight + "px";
+}
+
+  window.addEventListener("resize", resizeElement);
+  window.addEventListener("load", resizeElement);
